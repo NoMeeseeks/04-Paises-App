c67d856caa6e0673f87b268bfd1626612b4d349d
NoMeeseeks <Xavier.e.santamaria.ramirez@gmail.com>
2024-03-16 20:48:45 -0500

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
 
+    RouterModule,
     SharedModule
   ],
   providers: [],
